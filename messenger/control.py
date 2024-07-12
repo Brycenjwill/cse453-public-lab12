0fcdd9d0f40d20ca46afd3a03049531a71ef7168
Brycen Williams <114171494+Brycenjwill@users.noreply.github.com>
2024-07-12 12:35:43 -0600

    Update control.py

--- messenger/control.py
+++ messenger/control.py
@@ -8,3 +8,21 @@
 ########################################################################
 
 # you may need to put something here...
+
+# SECURITY_LEVELS dictionary
+SECURITY_LEVELS = {
+    'Public': 0,
+    'Confidential': 1,
+    'Privileged': 2,
+    'Secret': 3
+}
+ 
+# USER_SECURITY_LEVELS dictionary
+USER_SECURITY_LEVELS = {
+    'AdmiralAbe': SECURITY_LEVELS['Secret'],
+    'CaptainCharlie': SECURITY_LEVELS['Privileged'],
+    'SeamanSam': SECURITY_LEVELS['Confidential'],
+    'SeamanSue': SECURITY_LEVELS['Confidential'],
+    'SeamanSly': SECURITY_LEVELS['Confidential'],
+    'default': SECURITY_LEVELS['Public']
+}
